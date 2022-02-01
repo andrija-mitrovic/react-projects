f87bbb02a7ffdbc30bef8ef96f1defa9a02e4ee5
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-02-01 21:42:44 +0100

    Configure App component

--- menu/src/App.js
+++ menu/src/App.js
@@ -1,8 +1,32 @@
+import { useState } from "react";
+import Categories from "./components/Categories";
+import Menu from "./components/Menu";
+import items from "./data";
+
 function App() {
+  const allCategories = ["all", ...new Set(items.map((x) => x.category))];
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(items);
+      return;
+    }
+    var selectedItems = menuItems.filter((x) => x.category === category);
+    setMenuItems(selectedItems);
+  };
   return (
-    <div className="container">
-      
-    </div>
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
   );
 }
 
