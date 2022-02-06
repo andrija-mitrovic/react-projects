44803ae80ff69ed4aaf8555de18c4b1f52a96188
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-02-06 13:46:38 +0100

    Configure app component

--- tabs/src/App.js
+++ tabs/src/App.js
@@ -1,8 +1,73 @@
+import { useEffect, useState } from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
+
+const url = "https://course-api.com/react-tabs-project";
+
 function App() {
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [value, setValues] = useState(0);
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    setJobs(newJobs);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  if (loading) {
+    return (
+      <section className="section loading">
+        <h1>Loading...</h1>
+      </section>
+    );
+  }
+
+  const { company, dates, duties, title } = jobs[value];
+
   return (
-    <div className="App">
-      
-    </div>
+    <section className="section">
+      <div className="title">
+        <h2>experience</h2>
+        <div className="underline"></div>
+      </div>
+      <div className="jobs-center">
+        <div className="btn-container">
+          {jobs.map((item, index) => {
+            return (
+              <button
+                key={item.id}
+                onClick={() => setValues(index)}
+                className={`job-btn ${index === value && "active-btn"}`}
+              >
+                {item.company}
+              </button>
+            );
+          })}
+        </div>
+        <article className="job-info">
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className="job-date">
+            {duties.map((duty, index) => {
+              return (
+                <div key={index} className="job-desc">
+                  <FaAngleDoubleRight />
+                  <p>{duty}</p>
+                </div>
+              );
+            })}
+          </p>
+        </article>
+      </div>
+      <button type="button" className="btn">
+        more info
+      </button>
+    </section>
   );
 }
 
